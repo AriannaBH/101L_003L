9ff1eb17d096a872ce547ffd40cc5b66789f4c1b
AriannaBH <abh223d@gmail.com>
2021-10-12 22:12:42 -0500

    Main Menu Working in Lab6.py

---
+++ Assignments/Assign_6/Lab6.py
@@ -0,0 +1,13 @@
+while True:
+    print('MAIN MENU')
+    print('1) Encode a string')
+    print('2) Decode a string')
+    print('Q) Quit')
+    choice = input('Enter your selection ==> ')
+
+    if choice == '1':
+        print('one')
+    if choice == '2':
+        print('two')
+    if choice == 'Q' or choice == 'q':
+        print('quit')
