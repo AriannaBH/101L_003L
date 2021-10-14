1493946e280f30ab662e994309dd7e0e9893a0c7
AriannaBH <abh223d@gmail.com>
2021-10-14 16:18:28 -0500

    Finishes on Lab6.py

--- Assignments/Assign_6/Lab6.py
+++ Assignments/Assign_6/Lab6.py
@@ -6,8 +6,28 @@ while True:
     choice = input('Enter your selection ==> ')
 
     if choice == '1':
-        print('one')
+        phrase = input('Enter (brief) text to encrypt: ')
+        num = int(input('Enter the number to shift letters by: '))
+        enc=""
+        for i in phrase:
+            if(i>='a' and i<='z'):
+                enc+=chr((ord(i)-ord('a')+num)%26 + ord('a'))
+            elif(i>='A' and i<='Z'):
+                enc+=chr((ord(i)-ord('A')+num)%26 + ord('A'))
+            else:
+                enc+=i
+        print('Encrypted:', enc.upper())
     if choice == '2':
-        print('two')
+        phrase = input('Enter (brief) text to decrypt: ')
+        num = int(input('Enter the number to shift letters by: '))
+        enc=""
+        for i in phrase:
+            if(i>='a' and i<='z'):
+                enc+=chr((ord(i)-ord('a')-num)%26 + ord('a'))
+            elif(i>='A' and i<='Z'):
+                enc+=chr((ord(i)-ord('A')-num)%26 + ord('A'))
+            else:
+                enc+=i
+        print('Decrypted:', enc.upper())
     if choice == 'Q' or choice == 'q':
-        print('quit')
+        break
