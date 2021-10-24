92f7553409f45d2c76a08e9f3e5bb04a374cca2c
AriannaBH <abh223d@gmail.com>
2021-10-23 22:35:04 -0500

    menu function on Lab8.py mapped out

---
+++ Assignments/Assign_8/Lab8.py
@@ -0,0 +1,12 @@
+def menu():
+    print('            Grade Menu')
+    print('1 - Add Test')
+    print('2 - Remove Test')
+    print('3 - Clear Test')
+    print('4 - Add Assignments')
+    print('5 - Remove Assignments')
+    print('6 - Clear Assignments')
+    print('D - Display Scores')
+    print('Q - Quit')
+
+menu()
